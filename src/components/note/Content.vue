8e708a31f5b77775e938ce47efa75f38898514af
Matt Schlenker <m4ttschlenker@gmail.com>
2019-12-30 13:38:48 -0500

    Slims down Content to barebones.

--- src/components/note/Content.vue
+++ src/components/note/Content.vue
@@ -2,15 +2,6 @@
   <div class="Content">
     <div class="p-2 lg:p-12">
       <div class="w-full max-w-2xl px-16 py-10 rounded shadow m-auto bg-white">
-        <div class="mb-10">
-          <h1 class="text-3xl font-bold">
-            {{ note.title }}
-          </h1>
-          <p class="pb-8">
-            Total Reading time: {{ note.timeToRead }} Minutes
-          </p>
-          <hr class="border-indigo-800">
-        </div>
         <div
           class="text-gray-700 markdown-body"
           v-html="note.content"
