3201317a0156067b117961217ed1c2234a14fc62
Matt Schlenker <m4ttschlenker@gmail.com>
2019-12-31 03:36:12 -0500

    Disables search until I can yell at Algolia people.

--- src/components/header/AlgoliaSearch.vue
+++ src/components/header/AlgoliaSearch.vue
@@ -8,7 +8,8 @@
         height="18"
       />
       <input
-        placeholder="Search..."
+        disabled
+        placeholder="Search down while site is reindexed."
         class="w-full shadow-inner bg-gray-200 py-1 px-4 pl-10 rounded-lg h-10 outline-none border focus:border-indigo-800"
         aria-label="Search notes"
       >
