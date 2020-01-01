164048c86ecbc37b5cf7ef7b812c35d1241ab5c8
Matt Schlenker <m4ttschlenker@gmail.com>
2019-12-31 19:53:06 -0500

    Adds some google analytics.

--- gridsome.config.js
+++ gridsome.config.js
@@ -33,6 +33,12 @@ module.exports = {
         shouldTimeTravel: true,
         shouldPurgeUnusedKeyframes: true,
       }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-132810678-1'
+      }
     }
   ]
 }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,32 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
-import DefaultLayout from '~/layouts/Default.vue'
-import NoteLayout from '~/layouts/Note.vue'
-import VTooltip from 'v-tooltip'
-
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(fas)
 library.add(fab)
 library.add(far)
 
+import 'autotrack/lib/plugins/outbound-link-tracker';
+import 'autotrack/lib/plugins/max-scroll-tracker';
+
+ga('require', 'outboundLinkTracker')
+ga('require', 'maxScrollTracker')
+
 require('github-markdown-css/github-markdown.css')
 require('typeface-lato')
 
+import DefaultLayout from '~/layouts/Default.vue'
+import NoteLayout from '~/layouts/Note.vue'
+
+import VTooltip from 'v-tooltip'
+
 export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
-  Vue.component('font-awesome-icon', FontAwesomeIcon)
   Vue.use(VTooltip)
+  Vue.component('font-awesome-icon', FontAwesomeIcon)
   Vue.component('Layout', DefaultLayout)
   Vue.component('NoteLayout', NoteLayout)
 }
