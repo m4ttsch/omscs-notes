c632e78284a4702e6978cb166d3a885a6d08b3e7
Matt Schlenker <m4ttschlenker@gmail.com>
2020-01-03 02:04:56 -0500

    Cleans up header links and updates notes.

--- src/components/header/PrimaryNavLinks.vue
+++ src/components/header/PrimaryNavLinks.vue
@@ -1,41 +1,24 @@
 <template lang="html">
   <div class="PrimaryNavLinks">
     <g-link
-      to="/operating-systems/welcome"
-      class="nav-link"  
-      @click.native="$emit('click')"
+      v-for="(course, i) in courses"
+      :key="i"
+      :to="course.start"
+      class="mr-4 hover:text-indigo-800"
     >
-      GIOS
-    </g-link>
-    <g-link
-      to="/computer-networks/welcome"
-      class="nav-link"
-      @click.native="$emit('click')"
-    >
-      CN
-    </g-link>
-    <g-link
-      to="/information-security/welcome"
-      class="nav-link"
-      @click.native="$emit('click')"
-    >
-      IIS
+      {{ course.abbrev }}
     </g-link>
   </div>
 </template>
 
 <script>
+import { courses } from '~/../data/courses.json'
+
 export default {
-  name: 'PrimaryNavLinks'
+  name: 'PrimaryNavLinks',
+
+  data() {
+    return { courses }
+  }
 }
 </script>
-
-<style lang="scss" scoped>
-  .nav-link {
-    @apply mr-4;
-
-    &:hover {
-      @apply text-indigo-800;
-    }
-  }
-</style>
