db63b261edae86706caff168e789c892dfdd4299
Matt Schlenker <m4ttschlenker@gmail.com>
2020-01-04 01:48:38 -0500

    Gets search up and running yay.

--- src/components/header/AlgoliaSearch.vue
+++ src/components/header/AlgoliaSearch.vue
@@ -1,24 +1,56 @@
 <template lang="html">
-  <div class="w-full font-sans text-indigo-800 flex justify-end">
-    <label class="relative w-full">
-      <font-awesome-icon
-        icon="search"
-        class="m-3 fill-current absolute inset-0"
-        width="18"
-        height="18"
-      />
-      <input
-        disabled
-        placeholder="Search down while site is reindexed."
-        class="w-full shadow-inner bg-gray-200 py-1 px-4 pl-10 rounded-lg h-10 outline-none border focus:border-indigo-800"
-        aria-label="Search notes"
-      >
-    </label>
+  <div class="AlgoliaSearch">
+    <div class="w-full font-sans text-indigo-800 flex justify-end">
+      <label class="relative w-full">
+        <font-awesome-icon
+          icon="search"
+          class="m-3 fill-current absolute inset-0 z-10"
+          width="18"
+          height="18"
+        />
+        <input
+          id="algolia-search"
+          placeholder="Search..."
+          class="w-full shadow-inner bg-gray-200 py-1 px-4 pl-10 rounded-lg h-10 outline-none border focus:border-indigo-800"
+          aria-label="Search notes"
+        >
+      </label>
+    </div>
   </div>
 </template>
 
 <script>
+import docsearch from 'docsearch.js'
+import Analytics from '~/mixins/analytics'
+
 export default {
-  name: 'AlgoliaSearch'
+  name: 'AlgoliaSearch',
+  mixins: [Analytics],
+  mounted() {
+    docsearch({
+      apiKey: '9d9ff52895877e8baeac290c95e89704',
+      indexName: 'omscs-notes',
+      inputSelector: '#algolia-search',
+      debug: true,
+      handleSelected: (input, _, suggestion) => {
+        this.handleSearch(input, suggestion)
+      }
+    })
+  },
+
+  methods: {
+    handleSearch({ getVal }, { url }) {
+      this.logEvent('search', { search_term: getVal() })
+      window.open(url, '_self')
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+  @import 'docsearch.js/dist/npm/styles/main.scss';
+
+  .algolia-autocomplete {
+    width: 100%;
+  }
+</style>
